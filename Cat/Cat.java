2a1bc8656d36b817920a29bb8b1335ea13a5bf42
Stefan E. Mayer <stefan.mayer.80@gmail.com>
2015-02-07 17:37:05 +0000

    First commit.

---
+++ Cat/Cat.java
@@ -0,0 +1,34 @@
+import java.io.*;
+/**
+ * Write a description of class Cat here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Cat
+{
+    public static void main(String[] args) {
+        File currentDir = new File("./");
+        File file = new File(currentDir, args[0]);
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found.");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
