54357400d4862f1f9df22ee5a20c0861d761ea0f
Stefan E. Mayer <stefan.mayer.80@gmail.com>
2015-02-08 16:45:38 +0000

    Removed redundant character. Added message error message if directory already exists.

--- Mkdir/Mkdir.java
+++ Mkdir/Mkdir.java
@@ -8,10 +8,14 @@ import java.io.*;
 public class Mkdir
 {
     public static void main(String[] args) {
-        File currentDir = new File("./");
+        File currentDir = new File(".");
         for (int i = 0; i < args.length; i++) {
             File newDir = new File(currentDir, args[i]);
-            newDir.mkdir();
+            if (!newDir.exists()) {
+                newDir.mkdir();
+            } else {
+                System.out.println("Directory " + args[i] + " already exists.");
+            }
         }
     }
 }
